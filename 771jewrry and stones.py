8af63379cb929f6b75f41e6b314373a60c03b115
XINJIWANG <867753731@qq.com>
2019-03-31 15:34:57 +0800

    Add files via upload

---
+++ 771jewrry and stones.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        num = 0
+        for j in J:
+            num += S.count(j)
+        return num
+    
+class Solution2(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        return sum([i in J for i in S])
